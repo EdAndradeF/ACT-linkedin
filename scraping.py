6b5346a6ce4bdf507ed930dabb9ea7370885db31
EdAndradeF <edandradef@gmail.com>
2022-09-05 02:03:01 -0300

    persistencia cofig??

--- scraping.py
+++ scraping.py
@@ -1,8 +1,7 @@
-
-from functools import reduce
-from pickletools import read_string1
-from pydoc import describe
+from curses import meta
+from importlib.metadata import metadata
 from time import sleep
+from pathlib import Path
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 import dotenv
@@ -16,16 +15,19 @@ from selenium.common.exceptions import NoSuchElementException
 env = dotenv.load_dotenv('.env')
 
 
-def trat(func):
+def trat(func, args):
     try:
-        return func
+        r = func(*args)
+        return r
     except:
         print('deu erro')
-        class cao:
+        class Caô:
             text = None
-        return cao()
+            def get_attribute(self, lorota):
+                return None
+        return Caô()
 
-def is_in(elemento, item) -> list:
+def is_in(elemento, item) -> list: #not in uso 
     '''reduce function
         compara o item com elemento para defenir se item esta em elemento
         caso se item nao fizer parte de elemteno, item se torna o elemento 
@@ -40,23 +42,37 @@ def is_in(elemento, item) -> list:
     return elemento
 
 def linha0(tag:WebElement) -> dict:
-    tag1 = tag.find_element(By.CLASS_NAME, 'jobs-unified-top-card__subtitle-primary-grouping')
-    dic = {
-    'nome_empresa': trat(tag1.find_element(By.CLASS_NAME, 'jobs-unified-top-card__company-name')).text,
-    'localidade': trat(tag1.find_element(By.CLASS_NAME, 'jobs-unified-top-card__bullet')).text,
-    'local_trabalho': trat(tag1.find_element(By.CLASS_NAME,'jobs-unified-top-card__workplace-type')).text,
-    }
     
-    tag2 =tag.find_element(By.CLASS_NAME, 'jobs-unified-top-card__subtitle-secondary-grouping')
-    dic.update({
-        'data_post': trat(tag2.find_element(By.CLASS_NAME, 'jobs-unified-top-card__posted-date')).text,
-        'quantidade_inscritos': trat(tag2.find_element(By.CLASS_NAME, 'jobs-unified-top-card__bullet')).text,
-    })              
+    def getEle(tag:WebElement, colum_attr:dict):
+        d = {}
+        for key, vlr in colum_attr.items():
+             d[key] = trat(tag.find_element, (By.CLASS_NAME, vlr)).text
+        return d
+        
+    dic = {}
+    tag1 = tag.find_element(By.CLASS_NAME, 'jobs-unified-top-card__subtitle-primary-grouping')    
+    tag2 = tag.find_element(By.CLASS_NAME, 'jobs-unified-top-card__subtitle-secondary-grouping')
+
+    if tag1.text:
+        colum_attr1 = {
+            'nome_empresa': 'jobs-unified-top-card__company-name',
+            'localidade': 'jobs-unified-top-card__bullet',
+            'local_trabalho': 'jobs-unified-top-card__workplace-type'
+        }
+        dic.update(getEle(tag1, colum_attr1))     
+    
+    if tag2.text:
+        colum_attr2 = {
+            'data_post': 'jobs-unified-top-card__posted-date',
+            'quantidade_inscritos': 'jobs-unified-top-card__bullet'
+        }
+        dic.update(getEle(tag2, colum_attr2))
+        
     dic['perfil_empresa'] = None
     if dic['nome_empresa']:
         dic['perfil_empresa'] = trat(
-            tag1.find_element(By.TAG_NAME, 
-                              'a')).get_attribute('href')    
+            tag1.find_element, (By.TAG_NAME, 'a')
+            ).get_attribute('href')    
     return dic
 
 def recruter(tag:WebElement):
@@ -64,58 +80,40 @@ def recruter(tag:WebElement):
         return
     dic = {
         'nome_recrut': tag.find_element(By.TAG_NAME, 'strong').text, 
-        
+        'perfil_recrut': tag.find_element(By.TAG_NAME, 'a').get_attribute('href'),
+        'titulos_recrut': tag.find_element('class name', 'hirer-card__hirer-job-title').text
     }
-    r = tag.find_element(By.TAG_NAME, 'a')
-    return 
+    em_comum = tag.text.split('\n')[-1]
+    dic['qnt_conex_comum'] = int(em_comum.split()[0]) if 'em comum' in em_comum else None
+    return dic
 
 
 
 class LinkedIn:
 
     def __init__(self, window=True):
-        self.hoje = str(date.today().strftime("%d-%m-%y"))
+        self.hoje = date.today().strftime("%d-%m-%y")
         self.site = 'https://www.linkedin.com/'
         self.driver = Chrome(window=window)
         self.driver.get(self.site)
         self.login()
-        self.datavaga = []
+        self.datavaga = {}
         self.lista_idVagas = set()
         self.qnt_vagas = 0
 
     def __repr__(self) -> str:
-        return f'<LinkedIn.Scrapper>'
+        return f'linkedin-dados'
 
     def login(self):
+        '''
+
+        '''
         self.driver.find_element(By.ID,
                                  'session_key').send_keys(os.getenv('email'))
         self.driver.find_element(By.ID,
                                  'session_password').send_keys(os.getenv('senha'))
         self.driver.find_element(By.XPATH,
                                  '//*[@id="main-content"]/section[1]/div/div/form/button').click()
-
-    def conect(self, num=0):
-        self.pg = 1+num
-        self.driver.get(f'{self.site}search/results/people/?connectionOf=%5B%22ACoAACE0LVQBQqhtxtpGIVdhw-RFHGoCLQ6Z230%22%5D&network=%5B%22F%22%2C%22S%22%5D&origin=MEMBER_PROFILE_CANNED_SEARCH&sid=Frz&page={self.pg}')
-        # sleep(1.5)
-        self.pg_butao = self.driver.find_elements(By.TAG_NAME, "button")
-
-        for conex in self.pg_butao:
-            if conex.text == 'Conectar':
-                conex.click()
-                # sleep(1)
-                confirm = self.driver.find_elements(By.TAG_NAME, 'button')
-                for x in confirm:
-                    if x.text == 'Enviar':
-                        x.click()
-                        # sleep(1.5)
-                        break
-                    if x.text == 'Entendi':
-                        print('Limite aucancado')
-                        return
-
-        if self.pg < 100:
-            self.conect(self.pg)
 
     def vagas(self, busca, simples=True, r=2, inicio=0): 
         #todo refatorar!!!!!!#todo refatorar!!!!!!
@@ -163,55 +161,48 @@ class LinkedIn:
         title = 'h1' if job_page else 'h2'
         
         # sleep(3)
-        data = {'id': int(id)}
+        self.data = {id: {}}
             # conteiner de detalhes sobre a empresa
-        # top = self.driver.find_element(By.CLASS_NAME, 'jobs-unified-top-card')
+        top = self.driver.waitFindElement((By.CLASS_NAME, 'jobs-unified-top-card'), 'element')
 
-        # data['nome_vaga'] = top.find_element(By.TAG_NAME, title).text
-        
-        # emp_loc_dat = top.find_element(By.CLASS_NAME, 'jobs-unified-top-card__primary-description')
-        # data.update(linha0(emp_loc_dat))
+        self.data[id]['nome_vaga'] = top.find_element(By.TAG_NAME, title).text
+        print(self.data[id]['nome_vaga'])
+        emp_loc_dat = top.find_element(By.CLASS_NAME, 'jobs-unified-top-card__primary-description')
+        self.data[id].update(linha0(emp_loc_dat))
 
+        sleep(1)
+        # detalhes da empresa (tabela)
+        listas = top.find_elements(By.TAG_NAME, 'ul')
+        lis = []        
+        for tags in listas:
+            uls = [x.text for x in tags.find_elements(By.TAG_NAME, 'li')]
+            lis.append(uls) 
+        detalhes, *atividade = lis
+        self.data[id]['detalhes'] = detalhes
+        if atividade:
+            self.data[id]['estagio'] = atividade[0]
 
-        # # detalhes da empresa (tabela)
-        # listas = top.find_elements(By.TAG_NAME, 'ul')
-        # lis = []        
-        # for tags in listas:
-        #     uls = [x.text for x in tags.find_elements(By.TAG_NAME, 'li')]
-        #     lis.append(uls) 
-        # detalhes, atividade = lis
-        # data['detalhes'] = detalhes
-        # data['estagio'] = atividade[0]
+        # VAGA FECHADA
+        fechada = self.driver.find_element('class name', 'artdeco-inline-feedback__message')
+        self.data[id]['vaga_fechada'] = fechada.text if fechada else None
 
 
         #recrutador
-        recrutador_full = self.driver.find_element(By.CLASS_NAME, 'hirer-card__hirer-information')
-        data['recrutador'] = recruter(recrutador_full)
+        recrutador_full = self.driver.waitFindElement((By.CLASS_NAME, 'hirer-card__hirer-information'), 'element')
+        self.data[id]['recrutador'] = None
+        if isinstance(recrutador_full, WebElement):
+            print('tem recrutador')
+            self.data[id]['recrutador'] = recruter(recrutador_full)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-        data['empresa'] = 1
-        data['pagina_empresa'] = top.find_element(By.TAG_NAME, 'a').get_attribute('href')
-
-
+        # descricao
+        article = self.driver.find_element(By.TAG_NAME, 'article')
+        article.parent.find_element(By.CLASS_NAME, 'artdeco-card__actions').click()
+        self.data[id]['descricao'] = article.text
+        self.datavaga.update(self.data)
 
         '''
                             daqui pra baixo decrepitado 
-        '''
+        
 
         article = self.driver.find_element(By.CLASS_NAME, 'jobs-description__content')
 
@@ -264,13 +255,14 @@ class LinkedIn:
 
         data['descricao'] = article.find_element(By.ID, 'job-details').text
         self.datavaga.append(data)
-
+'''
 
     def setQnt(self, qnt:WebElement, text:str) -> None:
         '''
         remove texto especificado e devolve inteiro
+        caso o atributo qnt_vagas for 0
             qnt WebElement .> elemento com texto e numero
-            text str .> texto a ser retirado com o metodo .strip()
+                  text str .> texto a ser retirado com o metodo .strip()
         '''
         if not self.qnt_vagas:
             self.qnt_vagas = int(qnt.text.strip(text))
@@ -288,15 +280,19 @@ class LinkedIn:
         self.driver.get(f'{self.site}my-items/saved-jobs/?cardType=APPLIED&start={start}')
         sleep(2)
 
+        #quantidade de
         qnt = self.driver.find_element(By.CLASS_NAME, 'workflow-navigation__item')
         self.setQnt(qnt,'Minhas vagas\n')
 
+        # conteiner com lista de vagas
         vagas = self.driver.find_element(By.CLASS_NAME, 'workflow-results-container')
 
-        getCode = (lambda a: '/'.join(a.get_attribute('href').split('/')[5]))
+        # endereco das vagas
         lista_vagas = vagas.find_elements(By.TAG_NAME, 'a')
-        id_vagas = {getCode(a) for a in lista_vagas 
-                                if '/jobs/view/' in a.get_attribute('href')}
+                # id_vagas usa set para eliminar as duplicadas  
+        id_vagas = {a.get_attribute('href')
+                        for a in lista_vagas 
+                        if '/jobs/view/' in a.get_attribute('href')}
         self.lista_idVagas.update(id_vagas)
 
         if start < self.qnt_vagas:
@@ -310,37 +306,31 @@ class LinkedIn:
                         |.> de 10 em 10 
         '''
         self.minhasvagas(start)
-        conto = len(self.lista_idVagas)
+        conto = 0 # < len(self.lista_idVagas)
         for url in self.lista_idVagas:
-            print(conto, len(self.lista_idVagas))
-            self.percode(url)
+            print(conto, "/", len(self.lista_idVagas))
+            conto += 1
+            self.percode(url)     
 
-    def percode(self, idVg):
-        self.driver.get(self.site + 'jobs/view/' + idVg)
-        idVaga = idVg
-        while True:
-            try:
-                self.vaga_descricao(idVaga, job_page=True)
-                break
-            except NoSuchElementException:
-                sleep(1.5)
-
-
-
-        # lista_vagas = self.driver.find_elements(By.CLASS_NAME, 'app-aware-link')
-        # vagas_id = {link.get_attribute('href').split('/')[5] for link in lista_vagas if 'view' in link.get_attribute('href')}
-        # for id in vagas_id:
-        #     sleep(1.5)
-        #     print(len(self.datavaga))
-        #     vaga = f'{self.site}jobs/view/{id}/'
-        #     self.driver.get(vaga)
-        #     self.percode(id)
-        # if len(vagas_id) and not test:
-        #     self.minhasvagas(start=start+10)
-        # if not start:
-        #     with open(f'data/data_backpu_{self.hoje}.json', 'w', encoding='utf-8') as arq: 
-        #         json.dump(self.datavaga, arq, indent=4, ensure_ascii=False)
-        # return self.datavaga
+    def percode(self, Vg):
+        self.driver.get(Vg)
+        idVaga = Vg.split('/')[5] 
+        self.vaga_descricao(idVaga, job_page=True)
+        self.save_html(
+            self.data,
+            idVaga,
+            self.data[idVaga]['nome_vaga']
+        )   
+        
+    def save_json(self, path:str='data/lake/bronze/json'):
+        arq = Path(path) / f'{self.hoje}_linkedin_vagas.json'
+        with open(arq, 'w') as j:        
+            json.dump(j, self.datavaga)
+    
+    def save_html(self, id, nome, path:str='data/lake/bronze/html'):
+        arq = Path(path) / f"{self.hoje}_{nome} _{id}.html"
+        with open(arq, 'w') as html:
+            html.write(self.driver.page_source)
 
 
     def candidatura(self):
@@ -348,15 +338,39 @@ class LinkedIn:
         r = self.driver.find_element(By.TAG_NAME, 'footer').find_element(By.TAG_NAME, 'button').click()
         s = 21
 
+    def conect(self, num=0):
+        self.pg = 1+num
+        self.driver.get(f'{self.site}search/results/people/?connectionOf=%5B%22ACoAACE0LVQBQqhtxtpGIVdhw-RFHGoCLQ6Z230%22%5D&network=%5B%22F%22%2C%22S%22%5D&origin=MEMBER_PROFILE_CANNED_SEARCH&sid=Frz&page={self.pg}')
+        # sleep(1.5)
+        self.pg_butao = self.driver.find_elements(By.TAG_NAME, "button")
+
+        for conex in self.pg_butao:
+            if conex.text == 'Conectar':
+                conex.click()
+                # sleep(1)
+                confirm = self.driver.find_elements(By.TAG_NAME, 'button')
+                for x in confirm:
+                    if x.text == 'Enviar':
+                        x.click()
+                        # sleep(1.5)
+                        break
+                    if x.text == 'Entendi':
+                        print('Limite aucancado')
+                        return
+
+        if self.pg < 100:
+            self.conect(self.pg)
 
 
 if __name__ == '__main__':
     inicio = LinkedIn(window=False)
     # inicio.conect(7)
     # inicio.vagas('analista de dados')
-    inicio.percode('3251007156')
+    # inicio.percode('3212451203')
+    
     inicio.getFullVagas()
-    inicio.bye()
+    inicio.save_json()
+    inicio.driver.bye()
 
 
 
